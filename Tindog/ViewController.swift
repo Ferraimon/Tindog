a9c17cc81797435046a45de79676670dfa57d914
Ferran Rosales Lopera <ferranrosales@mac.com>
2018-06-26 09:09:20 +0200

    Initial Commit

---
+++ Tindog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tindog
+//
+//  Created by Ferran Rosales on 26/06/2018.
+//  Copyright © 2018 Parets Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
